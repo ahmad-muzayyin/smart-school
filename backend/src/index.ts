180d0ed7d9c1df2bf0e838b4c7c228a2c7bd1ffa
ahmad-muzayyin <Muzayyin.rpl@gmail.com>
2026-01-31 12:01:07 +0700

    Fix CORS for Web PATCH requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,11 @@ const app = express();
 
 // Middlewares
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+}));
 app.use(morgan('dev'));
 app.use(express.json({ limit: '10mb' }));
 
