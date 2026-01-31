36103721ba1e3106e0488d9473e812eab12eb091
ahmad-muzayyin <Muzayyin.rpl@gmail.com>
2026-02-01 00:09:14 +0700

    fix: replace Stack Navigator with Native Stack (no Reanimated needed)

--- frontend/src/navigation/RootNavigator.tsx
+++ frontend/src/navigation/RootNavigator.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { useAuthStore } from '../store/useAuthStore';
 import { ActivityIndicator, View } from 'react-native';
 
@@ -47,7 +47,7 @@ import LanguageSettingsScreen from '../screens/common/LanguageSettingsScreen';
 import PrivacySettingsScreen from '../screens/common/PrivacySettingsScreen';
 import PrivacyPolicyScreen from '../screens/common/PrivacyPolicyScreen';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function RootNavigator() {
     const { token, user, isLoading, isInitialized } = useAuthStore();
